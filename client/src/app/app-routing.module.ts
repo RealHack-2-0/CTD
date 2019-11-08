a570c02c39e25edb2a39f281a050f554cce80ac5
charindukavishan <charindukavishan@gmail.com>
2019-11-08 06:47:37 +0530

    last commit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AuthGuard } from './auth/auth.guard';
+import { RegisComponent } from './regis/regis.component';
+import { PostComponent } from './post/post.component';
 
-const routes: Routes = [];
+export const appRoutes: Routes = [
+
+  {path: 'login', component: SignInComponent,},
+  {path: 'userprofile', component: UserProfileComponent,canActivate:[AuthGuard]},
+  {path:'regis',component:RegisComponent},
+  {path:'post/:id',component:PostComponent}
+  
+  //{ path: '**', component: SignInComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes 
+   // ,{ useHash: true}
+   )],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }           from '@angular/core';
+import { BrowserModule }      from '@angular/platform-browser';
+import { FormsModule }        from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+/* App Root */
+import { AppComponent }       from './app.component';
+import { NavbarComponent }    from './navbar/navbar.component';
+
+
+/* Routing Module */
+import { AppRoutingModule }   from './app-routing.module';
+//import {appRoutes }   from './routing';
+//import { RouterModule } from '@angular/router';
+
+import { RegserviceService } from './servers/regservice.service';
+import { HttpModule } from '@angular/http';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
+
+
+import { AuthGuard } from './auth/auth.guard';
+import { AuthInterceptor } from './auth/auth.interceptor';
+import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RegisComponent } from './regis/regis.component';
+import { AddpostComponent } from './user-profile/addpost/addpost.component';
+import { PostComponent } from './post/post.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { RatingComponent } from './post/rating/rating.component';
+@NgModule({ 
+    imports:      [ BrowserModule, 
+                    FormsModule,
+                    AppRoutingModule,
+                    HttpModule, 
+                    HttpClientModule,
+                    FlashMessagesModule,
+                    AngularFireModule.initializeApp({
+                        apiKey: "AIzaSyD3dyNOte2kt3vQd1KjhivBe9Ahc3dtCiY",
+                        authDomain: "parkherefiles.firebaseapp.com",
+                        storageBucket: "parkherefiles.appspot.com",
+                        projectId: "parkherefiles",
+                      }),
+                      AngularFireStorageModule,
+                    //RouterModule.forRoot(appRoutes),
+                  ],
+    providers:    [{
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptor,
+        multi: true
+      },AuthGuard ,FlashMessagesService,RegserviceService],
+    declarations: [ AppComponent, NavbarComponent,  SignInComponent, UserProfileComponent, RegisComponent, AddpostComponent, PostComponent, SearchBarComponent, RatingComponent],
+    bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+ 
+export class AppModule {}
